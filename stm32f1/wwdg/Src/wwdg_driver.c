33f748353d6fb22add7ddc9834c614573495bec7
emauriciorg@gmail.com <emauriciorg@gmail.com>
2019-12-06 23:01:32 -0500

    feat:added macros to calculate WWDG window value

--- stm32f1/wwdg/Src/wwdg_driver.c
+++ stm32f1/wwdg/Src/wwdg_driver.c
@@ -4,15 +4,31 @@
 #include "stm32f1xx_hal.h"
 
 /*MAX value for iwdg time is 26.21*/
+//WIDNOW_VALUE_IN_MS
+
+/*
+	RESET CONDITIONS
+	downcounter goes below 0x40
+	downcounter is reloaded outsite the window
+
+	ISR
+	Early wake interrupt when the downcounter is equal to 0x40
+*/
+
 #define WATCHDOG_PERIOD_IN_SEC 25.6
-#define WATCHDOG_PRESCALER     IWDG_PRESCALER_256
+#define WATCHDOG_PRESCALER     WWDG_PRESCALER_2
 #define LOW_FREQ_OSC           40000
 #define WATCDOG_AUTO_RELOAD_VALUE  ((WATCHDOG_PERIOD_IN_SEC/WATCHDOG_PRESCALER) *LOW_FREQ_OSC)
+
+#define WDT_ENABLE_BIT     0x08
+#define WIDNOW_VALUE_IN_MS 100
+
 
 typedef struct {
 	uint32_t window_ms;
 	uint8_t  enabled;
 } soft_watchdog_t;
+
 
 volatile soft_watchdog_t soft_wdt;
 
@@ -20,7 +36,6 @@ WWDG_HandleTypeDef hwwdg;
 
 
 static void setup_stm_wdt(void);
-static void init_reset_pin(void);
 
 
 void HAL_WWDG_EarlyWakeupCallback(WWDG_HandleTypeDef *hwwdg)
@@ -77,18 +92,6 @@ static void setup_stm_wdt(void) /*taken from stmcubemx*/
 }
 
 
-static void init_reset_pin(void)
-{
-	GPIO_InitTypeDef   GPIO_InitStruct = {0};
-	GPIO_InitStruct.Pin   = WATCHDOG_RESET_PIN;
-	GPIO_InitStruct.Pull  = GPIO_PULLUP;
-	GPIO_InitStruct.Mode  = GPIO_MODE_INPUT;
-	GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
-	DISABLE_SYSTEM_RESET;
-
-	HAL_GPIO_DeInit(WATCHDOG_RESET_PORT, WATCHDOG_RESET_PIN);
-	HAL_GPIO_Init(WATCHDOG_RESET_PORT, &GPIO_InitStruct);
-}
 
 void watchdog_enable(void)
 {
